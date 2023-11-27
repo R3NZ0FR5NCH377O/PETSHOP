3d3ef49c94bacd9aca1344929b7be8881372dc2f
pink0x <superpinkox@gmail.com>
2023-11-27 11:36:50 -0300

    Cambie el mapa y saque el scroll horizontal

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -12,7 +12,22 @@ tailwind.config = {
           blanco: "#DAFFFB",
         }
       }
-    }
+    },
+
+    plugins: [
+      function ({ addUtilities }) {
+          const newUtilities = {
+              ".no-scrollbar::-webkit-scrollbar": {
+                  display: "none",
+              },
+              ".no-scrollbar": {
+                  "-ms-overflow-style": "none",
+                  "scrollbar-width": "none",
+              },
+          };
+          addUtilities(newUtilities);
+      },
+  ],
   };
 
   
